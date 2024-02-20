1a2ccfd667131c2050d843afcaee2099559d2b4c
Markvi268 <markku.viertokangas@edu.savonia.fi>
2024-02-20 13:45:40 +0200

    add helpers

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_package",
+    version="0.0.1",
+    author="Markku Viertokangas",
+    author_email="markku.viertokangas@hotmail.com",
+    description="Contains helper functions for unit testing",
+    long_description="test_package is a package that contains helper functions for unit testing ",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+
+    python_requires=">=3.6",
+
+)
